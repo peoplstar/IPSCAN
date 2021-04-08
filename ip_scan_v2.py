c7a5baac827eb1f59569d52e58e316a5bdefd17f
peoplstar <78135526+peoplstar@users.noreply.github.com>
2021-04-08 18:44:19 +0900

    Threading Scanner

--- ip_scan_v2.py
+++ ip_scan_v2.py
@@ -1,19 +1,37 @@
 import netaddr
-import pingCheck_v2 as pc
-#isUp(host) : pingCheck_v2 Function
+import os
+import threading
 
-iplist = "192.168.203.0/24"
+iplist = "172.30.1.22/24"
+threadLock = threading.Lock()
+threads = []
+
 path = "C:/Users/asxz3/OneDrive/바탕 화면/IPScan/"
 
-# iplist_file = open(path+"iplist_v2.txt", "w", encoding="utf8")
+iplist_file = open(path + "iplist_v2.txt", "w", encoding="utf8")
+
+
+print("Start Scan")
+def IPScan(ip):
+    try:
+        response = os.system("ping -n 1 " + ip)
+        if not response:
+            iplist_file.write("Alive Host :" + str(ip) + "\n")
+            print("Network Active")
+            threadLock.acquire()
+            print("Alive Host  :" + str(ip))
+            threaLock.release()
+    except:
+        pass
 
 for ip in netaddr.IPNetwork(iplist):
     ip = str(ip)
-    th = pc.threading.Thread(target = pc.isUp(ip), args = (ip,))
+    th = threading.Thread(target = IPScan, args = (ip,))
     th.start()
-    pc.threads.append(th)
+    threads.append(th)
 
-for j in pc.threads:
-    j.join()
+for t in threads:
+    t.join()
 
-# iplist_file.close()
+iplist_file.close()
+print("End Scan")
