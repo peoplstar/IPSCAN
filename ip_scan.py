4a5df00432516445584d7b75387bcd8d22357035
peoplstar <78135526+peoplstar@users.noreply.github.com>
2021-04-08 16:07:25 +0900

    IPscanner

--- ip_scan.py
+++ ip_scan.py
@@ -7,7 +7,9 @@ iplist = "192.168.203.0/24"
 path = "C:/Users/asxz3/OneDrive/바탕 화면/IPScan/"
 #print(pc.isUp("192.168.203.128"))
 
-iplist_file = open(path+"iplist.txt", "w", encoding="utf8")
+iplist_file = open(path + "iplist.txt", "w", encoding="utf8")
 for ip in netaddr.IPNetwork(iplist):
     if pc.isUp(str(ip)):
         iplist_file.write("Alive Host :" + str(ip) + "\n")
+
+iplist_file.close()
