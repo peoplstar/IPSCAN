9cb76eb7ec3a375ebda42d01e52978de085eaa50
peoplstar <78135526+peoplstar@users.noreply.github.com>
2021-04-08 18:26:41 +0900

    Test Scanner

--- ip_scan_v2.py
+++ ip_scan_v2.py
@@ -5,17 +5,15 @@ import pingCheck_v2 as pc
 iplist = "192.168.203.0/24"
 path = "C:/Users/asxz3/OneDrive/바탕 화면/IPScan/"
 
-iplist_file = open(path+"iplist_v2.txt", "w", encoding="utf8")
+# iplist_file = open(path+"iplist_v2.txt", "w", encoding="utf8")
 
 for ip in netaddr.IPNetwork(iplist):
     ip = str(ip)
-    if pc.isUp(ip):
-        th = pc.threading.Thread(target = pc.isUp(ip), args = (ip,))
-        th.start()
-        pc.threads.append(th)
-        iplist_file.write("Alive Host :" + ip + "\n")
+    th = pc.threading.Thread(target = pc.isUp(ip), args = (ip,))
+    th.start()
+    pc.threads.append(th)
 
 for j in pc.threads:
     j.join()
 
-iplist_file.close()
+# iplist_file.close()
--- pingCheck_v2.py
+++ pingCheck_v2.py
@@ -8,6 +8,10 @@ threads = []
 print("Start Scan")
 def isUp(hostname):
     if platform.system() == "Windows":
+        threadLock.acquire()
         response = os.system("ping " + hostname + " -n 1")
+        threadLock.release()
     else:
+        threadLock.acquire()
         response = os.system("ping -c 1 " + hostname)
+        threadLock.release()
